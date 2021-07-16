0756b32aa2349a13a5abea7402bb459b511764d1
weiqian <weiqian@beisen.com>
2021-07-16 19:12:09 +0800

    fix: 学习使用lint-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "settings": {
+    "react":{
+      "version":"detect"
+    }
+  },
+  "env":{
+    "browser":true,
+    "node":true,
+    "es6":true
+  },
+  "plugins":["react"],
+  "parser": "babel-eslint"
+};
--- src/components/cmp1/index.js
+++ src/components/cmp1/index.js
@@ -5,7 +5,56 @@ class FirstChildCmp extends React.Component {
     super(props);
   }
   render() {
-    return <div>我是第一个child</div>;
+    var Tv = {
+      open: function () {
+        alert('open');
+      },
+      close: function () {
+        alert('close');
+      }
+    };
+    var createCommand = function (receiver) {
+      // var execute = function () {
+      //   return receiver.open();
+      // };
+      // var undo = function () {
+      //   return receiver.close();
+      // };
+      // var execute = receiver.open
+      return {
+        execute: receiver.open,
+        undo: receiver.close
+      };
+    };
+    // var setCommand = function (command) {
+    //   document.getElementById('execute').onclick = function () {
+    //     command.execute();
+    //   };
+    //   document.getElementById('undo').onclick = function () {
+    //     command.undo();
+    //   };
+    // };
+    // setCommand(createCommand(Tv));
+    var command = createCommand(Tv);
+    return (
+      <div>
+        我是第一个child
+        <button
+          id='execute'
+          onClick={() => {
+            command.execute();
+          }}>
+          open
+        </button>
+        <button
+          id='undo'
+          onClick={() => {
+            command.undo();
+          }}>
+          close
+        </button>
+      </div>
+    );
   }
 }
 
